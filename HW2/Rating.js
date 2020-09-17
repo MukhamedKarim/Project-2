a2e2d01c0551d135aa35c40ec5004ca82e194987
MukhamedKarim <62872580+MukhamedKarim@users.noreply.github.com>
2020-09-17 20:48:28 +0600

    Add files via upload

---
+++ HW2/Rating.js
@@ -0,0 +1,200 @@
+//clikc 1
+function fun1(){
+  document.querySelector('#change').innerHTML = "1★";
+}
+function fun2(){
+  document.querySelector('#change').innerHTML = "2★";
+}
+function fun3(){
+  document.querySelector('#change').innerHTML = "3★";
+}
+function fun4(){
+  document.querySelector('#change').innerHTML = "4★";
+}
+function fun5(){
+  document.querySelector('#change').innerHTML = "5★";
+}
+
+
+
+
+
+//clikc 2
+function fun6(){
+  document.querySelector('#change2').innerHTML = "1★";
+}
+function fun7(){
+    document.querySelector('#change2').innerHTML = "2★";
+  }
+  function fun8(){
+    document.querySelector('#change2').innerHTML = "3★";
+  }
+  function fun9(){
+    document.querySelector('#change2').innerHTML = "4★";
+  }
+  function fun10(){
+    document.querySelector('#change2').innerHTML = "5★";
+  }   
+
+
+
+
+
+  //clikc 3
+function fun11(){
+    document.querySelector('#change3').innerHTML = "1★";
+  }
+  function fun12(){
+      document.querySelector('#change3').innerHTML = "2★";
+    }
+    function fun13(){
+      document.querySelector('#change3').innerHTML = "3★";
+    }
+    function fun14(){
+      document.querySelector('#change3').innerHTML = "4★";
+    }
+    function fun15(){
+      document.querySelector('#change3').innerHTML = "5★";
+    }   
+
+
+
+
+
+    //clikc 4
+function fun16(){
+    document.querySelector('#change4').innerHTML = "1★";
+  }
+  function fun17(){
+      document.querySelector('#change4').innerHTML = "2★";
+    }
+    function fun18(){
+      document.querySelector('#change4').innerHTML = "3★";
+    }
+    function fun19(){
+      document.querySelector('#change4').innerHTML = "4★";
+    }
+    function fun20(){
+      document.querySelector('#change4').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+    //clikc 5
+function fun21(){
+    document.querySelector('#change5').innerHTML = "1★";
+  }
+  function fun22(){
+      document.querySelector('#change5').innerHTML = "2★";
+    }
+    function fun23(){
+      document.querySelector('#change5').innerHTML = "3★";
+    }
+    function fun24(){
+      document.querySelector('#change5').innerHTML = "4★";
+    }
+    function fun25(){
+      document.querySelector('#change5').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+    //clikc 6
+function fun26(){
+    document.querySelector('#change6').innerHTML = "1★";
+  }
+  function fun27(){
+      document.querySelector('#change6').innerHTML = "2★";
+    }
+    function fun28(){
+      document.querySelector('#change6').innerHTML = "3★";
+    }
+    function fun29(){
+      document.querySelector('#change6').innerHTML = "4★";
+    }
+    function fun30(){
+      document.querySelector('#change6').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+
+    //clikc 7
+function fun31(){
+    document.querySelector('#change7').innerHTML = "1★";
+  }
+  function fun32(){
+      document.querySelector('#change7').innerHTML = "2★";
+    }
+    function fun33(){
+      document.querySelector('#change7').innerHTML = "3★";
+    }
+    function fun34(){
+      document.querySelector('#change7').innerHTML = "4★";
+    }
+    function fun35(){
+      document.querySelector('#change7').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+
+    //clikc 8
+function fun36(){
+    document.querySelector('#change8').innerHTML = "1★";
+  }
+  function fun37(){
+      document.querySelector('#change8').innerHTML = "2★";
+    }
+    function fun38(){
+      document.querySelector('#change8').innerHTML = "3★";
+    }
+    function fun39(){
+      document.querySelector('#change8').innerHTML = "4★";
+    }
+    function fun40(){
+      document.querySelector('#change8').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+
+    //clikc 9
+function fun41(){
+    document.querySelector('#change9').innerHTML = "1★";
+  }
+  function fun42(){
+      document.querySelector('#change9').innerHTML = "2★";
+    }
+    function fun43(){
+      document.querySelector('#change9').innerHTML = "3★";
+    }
+    function fun44(){
+      document.querySelector('#change9').innerHTML = "4★";
+    }
+    function fun45(){
+      document.querySelector('#change9').innerHTML = "5★";
+    }   
+
+
+
+
+
+
+
+    
